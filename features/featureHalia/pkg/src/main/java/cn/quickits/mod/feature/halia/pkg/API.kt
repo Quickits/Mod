7e195276684fa0d4c380c2f140d123818610e5d7
Gavin Liu <gavin6liu@gmail.com>
2020-05-04 16:02:26 +0800

    feat: halia simple demo

---
+++ features/featureHalia/pkg/src/main/java/cn/quickits/mod/feature/halia/pkg/API.kt
@@ -0,0 +1,19 @@
+package cn.quickits.mod.feature.halia.pkg
+
+import io.reactivex.Observable
+import java.util.concurrent.TimeUnit
+
+
+/**
+ * @program: Halia
+ * @description:
+ * @author: gavinliu
+ * @create: 2019-04-22 15:52
+ **/
+object API {
+
+    fun loadingByNetwork(): Observable<String> {
+        return Observable.just("Hello, 123").delay(2, TimeUnit.SECONDS)
+    }
+
+}
--- features/featureHalia/pkg/src/main/java/cn/quickits/mod/feature/halia/pkg/HaliaDemoActivity.kt
+++ features/featureHalia/pkg/src/main/java/cn/quickits/mod/feature/halia/pkg/HaliaDemoActivity.kt
@@ -1,7 +1,9 @@
 package cn.quickits.mod.feature.halia.pkg
 
 import android.os.Bundle
+import android.util.Log
 import cn.quickits.arch.base.app.BaseActivity
+import cn.quickits.halia.loading
 import kotlinx.android.synthetic.main.activity_halia_demo.*
 
 /**
@@ -15,7 +17,13 @@ class HaliaDemoActivity : BaseActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_halia_demo)
         button.setOnClickListener {
-
+            API.loadingByNetwork()
+                .loading()
+                .subscribe({ content ->
+                    Log.d(HaliaDemoActivity::class.simpleName, content)
+                }, { error ->
+                    Log.e(HaliaDemoActivity::class.simpleName, error.message ?: "")
+                })
         }
 
     }
